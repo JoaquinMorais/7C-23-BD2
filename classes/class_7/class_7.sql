767e14f20b9e27c78eaf201df7972dc10a4d13be
JoaquinMorais <joaquinmorais2005@gmail.com>
2023-05-09 22:48:59 -0300

    classe 7, actividad n1, n2 y n3

---
+++ classes/class_7/class_7.sql
@@ -0,0 +1,53 @@
+1 Find the films with less duration, show the title and rating.
+
+2 Write a query that returns the title of the film which duration is the lowest. 
+If there are more than one film with the lowest durtation, the query returns an empty resultset.
+
+3 Generate a report with list of customers showing the lowest payments done by each of them. 
+Show customer information, the address and the lowest amount, 
+provide both solution using ALL and/or ANY and MIN.
+
+4 Generate a report that shows the customer's information with the highest payment and the lowest 
+payment in the same row.
+'
+
+
+use sakila;
+
+
+/*  1  */
+SELECT title,rating,length FROM film 
+WHERE length <= ALL (
+  SELECT length 
+  FROM film
+);
+
+/*  2  */
+SELECT title, rating, length
+FROM film AS f1
+WHERE length <= ALL (
+  SELECT length 
+  FROM film
+)
+AND NOT EXISTS(
+  SELECT * FROM film AS f2 WHERE f2.film_id <> f1.film_id AND f2.length <= f1.length
+);
+
+/*  3  */
+Generate a report with list of customers showing the lowest payments done by each of them. 
+Show customer information, the address and the lowest amount, 
+provide both solution using ALL and/or ANY and MIN.
+
+
+
+SELECT * from (
+  SELECT c.customer_id as id,c.first_name, c.last_name, p.amount AS lowest_payment
+  FROM customer c
+  INNER JOIN payment p on c.customer_id = p.customer_id
+  INNER JOIN address a on c.address_id = a.address_id
+  WHERE p.amount <= ALL(
+    SELECT amount from payment WHERE customer_id=c.customer_id
+  )
+) as queryPapa
+GROUP BY id,lowest_payment;
+
